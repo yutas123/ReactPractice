a46aaa88464a21f92edf9e03af0699f8b89491c5
小林勇太崇 <kobayashiyut@pivot.jp>
2025-05-01 14:12:38 +0900

    配列

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
-  let num = 0
-  const onClickButton = () => {
-    num +=1
-    console.log(num)
+    // useState(0)の戻り値をコンソールに出力
+    const stateArray = useState(0);
+    console.log('useState(0)の戻り値:', stateArray);
+    
+    // 通常の分割代入を使用
+    const [num,setNum] = useState(0)
+    const onClickButton = () => {
+      setNum(num+1)
+      console.log(num)
+    }
+    return (
+      <>
+        {num}
+        <button onClick={onClickButton}>+1</button>
+      </>
+    )
   }
-  return (
-    <>
-      {num}
-      <button onClick={onClickButton}>+1</button>
-    </>
-  )
-}
 
 export default Counter;
