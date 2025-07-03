900e6ef8c25e6e8e7a343184513d9a25c9fe1790
小林勇太崇 <kobayashiyut@pivot.jp>
2025-07-03 14:52:08 +0900

    リファクタ

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,26 @@
-const user = {
+interface ProfileProps {
+  textColor?: string;
+  user?: {
+    name: string;
+    imageUrl: string;
+    imageSize: number;
+  };
+}
+
+const defaultUser = {
   name: 'Hedy Lamarr',
   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
   imageSize: 90,
 };
 
-export default function Profile({ textColor }: { textColor: any }) {
+export default function Profile({ textColor = 'black', user = defaultUser }: ProfileProps) {
   return (
     <>
-      <h1 style={{ color: textColor || 'black' }}>{user.name}</h1>
+      <h1 style={{ color: textColor }}>{user.name}</h1>
       <img
         className="avatar"
         src={user.imageUrl}
-        alt={'Photo of ' + user.name}
+        alt={`Photo of ${user.name}`}
         style={{
           width: user.imageSize,
           height: user.imageSize
