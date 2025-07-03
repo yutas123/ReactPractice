5048afb969173eb95b96bd081a895eee16f7bbcb
小林勇太崇 <kobayashiyut@pivot.jp>
2025-07-03 16:35:40 +0900

    List

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,18 +1,21 @@
 const products = [
-  { title: 'hoge', isFruit: false, id: 1 },
+  { title: 'A', isFruit: false, id: 1 },
+  { title: 'B', isFruit: false, id: 2 },
+  { title: 'C', isFruit: true, id: 3 },
+  { title: 'D', isFruit: false, id: 4 },
+  { title: 'E', isFruit: false, id: 5 },
 ];
 
 export function ShoppissssddngList() {
   const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
+    <li key={product.id}>
       {product.title}
+      {product.isFruit}
+      {product.id}
     </li>
   );
+
+  console.log(listItems);
 
   return (
     <ul>{listItems}</ul>
